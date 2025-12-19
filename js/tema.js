3d88de9cb208589a994776815fc518049c88a43b
Richi <shikimakayama@gmail.com>
2025-12-18 21:56:55 -0500

    Tareas De Tecnologias Web

---
+++ js/tema.js
@@ -0,0 +1,15 @@
+const boton = document.getElementById("temaBtn");
+
+if (localStorage.getItem("tema") === "oscuro") {
+  document.body.classList.add("oscuro");
+  boton.textContent = "☀️ Modo claro";
+}
+
+boton.addEventListener("click", () => {
+  document.body.classList.toggle("oscuro");
+
+  const tema = document.body.classList.contains("oscuro") ? "oscuro" : "claro";
+  localStorage.setItem("tema", tema);
+
+  boton.textContent = tema === "oscuro" ? "☀️ Modo claro" : "🌙 Modo oscuro";
+});
